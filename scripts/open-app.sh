907fd52db7c29fd117be385dfff1a8653a73b16e
Sanghyun Lee <hammerha@gmail.com>
2022-11-29 07:59:19 +0100

    Make it open only when the app is running

--- scripts/open-app.sh
+++ scripts/open-app.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-open "$(cat "$HOME/.sang_storage/active-app")"
+
+APP_PATH="$(cat "$HOME/.sang_storage/active-app")"
+
+if ps -ef | grep "$APP_PATH" | grep -v grep > /dev/null; then
+  open "$APP_PATH"
+else
+  echo "$APP_PATH is not running"
+
+  terminal_notifier=/usr/local/bin/terminal-notifier
+  if [ -f "$terminal_notifier" ]; then
+    "$terminal_notifier" -title "App is not running" -message "$APP_PATH" -timeout 5 &
+  else
+    echo "Can't find $terminal_notifier"
+  fi
+fi
+
